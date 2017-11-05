f03b2625baaf22a8e5b63e37a07b93191377da23
Ryan Pfeifer <pfeifer.ryan@gmail.com>
2017-11-05 21:51:56 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,20 @@
 # Write your code here.
+def line(queue)
+  if queue.size > 0
+    puts queue
+  else
+    puts "The line is empty"
+  end
+end
+
+def take_a_number(queue, person)
+    queue << person
+end
+
+def now_serving(queue)
+  if queue.size > 0
+    puts queue[0]
+  else
+    puts "The line is empty"
+  end
+end
