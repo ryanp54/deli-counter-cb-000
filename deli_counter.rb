ee7498f784140d2685540a7d3256e2ef357552e0
Ryan Pfeifer <pfeifer.ryan@gmail.com>
2017-11-05 22:02:40 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,8 +15,8 @@ end
 
 def now_serving(queue)
   if queue.size > 0
-    puts queue[0]
+    puts "Currently serving #{queue.shift}."
   else
-    puts "The line is currently empty"
+    puts "There is nobody waiting to be served!"
   end
 end
