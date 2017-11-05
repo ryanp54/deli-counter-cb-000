802ba75c30430ed53365617e7b0025fbcba01b99
Ryan Pfeifer <pfeifer.ryan@gmail.com>
2017-11-05 22:03:17 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,22 @@
 # Write your code here.
+def line(queue)
+  if queue.size > 0
+    verbose_line = ":" + queue.collect {|person| " #{queue.index(person)+1}. #{person}"}.join
+  else
+    verbose_line = " empty."
+  end
+  puts "The line is currently#{verbose_line}"
+end
+
+def take_a_number(queue, person)
+    puts "Welcome, #{person}. You are number #{queue.size+1} in line."
+    queue << person
+end
+
+def now_serving(queue)
+  if queue.size > 0
+    puts "Currently serving #{queue.shift}."
+  else
+    puts "There is nobody waiting to be served!"
+  end
+end
